c1e5c538494e1bc256657a3fbed5b362a8fd289f
Arash <arash.d83@gmail.com>
2024-04-01 12:08:43 -0500

    folder structure added

---
+++ template.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "disease_classifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir = filepath.parent
+    filename = filepath.name
+
+    if not filedir.is_dir():
+        filedir.mkdir(parents=True, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        with open(filepath, "w") as f:
+            f.write("")  # Write an empty string to create an empty file
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
+
